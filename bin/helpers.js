7486a165d53e84ac656f3e6c4674db75854d16f5
Anthony <keppi@o2.pl>
2017-11-21 20:34:27 +0100

    Add files via upload

--- bin/helpers.js
+++ bin/helpers.js
@@ -1,6 +1,52 @@
 const chalk = require('chalk');
 const pkg = require('../package.json');
 
+const version = () => {
+	console.log(pkg.version);
+	process.exit(1);
+};
+
+const help = () => {
+	console.log(chalk.yellow(`
+Usage:
+
+ $ cash <amount> <currency>
+
+ $ cash <command>
+
+Commands:
+--help        Display help message
+--version     Display version number
+
+Currencies:
+ • usd
+ • pln
+ • rub
+ • aud
+ • chf
+ • eur
+ • bgn
+ • jpy
+ • cad
+ • try
+ • mxn
+
+ Full list of currencies: http://akep.us/currencies
+
+Examples:
+
+ $ cash 1 usd
+
+ $ cash 1 usd eur pln aud
+
+ $ cash --help
+
+ Cash CLI comes with no warranty.
+ The rates are updated daily around 4PM CET every working day.
+  `));
+	process.exit(1);
+};
+
 const helpers = argv => {
   // Version
 	if (argv.indexOf('--version') !== -1 || argv.indexOf('-v') !== -1) {
@@ -11,30 +57,6 @@ const helpers = argv => {
 	if (argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1 || !argv.length) {
 		help();
 	}
-};
-
-const version = () => {
-	console.log(pkg.version);
-	process.exit(1);
-};
-
-const help = () => {
-	console.log(chalk.yellow(`
-  Usage:
-
-    $ cash <amount> <currency>
-
-  Some currencies: usd, pln, rub, aud, chf, eur
-
-  Examples:
-
-    $ cash 1 usd
-
-    or
-
-    $ cash 1 usd eur pln aud
-  `));
-	process.exit(1);
 };
 
 module.exports = helpers;

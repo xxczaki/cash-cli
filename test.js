0c03024c421eb5209dd55c8b96f4559bc00cecd5
Antoni Kepinski <xxczaki@pm.me>
2019-06-26 23:17:37 +0200

    fix: remove all `execa.shell` instances and replace them with `execa`

--- test.js
+++ test.js
@@ -8,37 +8,37 @@ const config = new Conf();
 config.clear();
 
 test('--help', async t => {
-	const ret = await execa.shell('node ./bin/index.js --help');
+	const ret = await execa('./bin/index.js', ['--help']);
 	t.regex(ret.stdout, /Examples/);
 });
 
 test('--version', async t => {
-	const {stdout} = await execa.shell('node ./bin/index.js --version');
+	const {stdout} = await execa('./bin/index.js', ['--version']);
 	t.true(typeof stdout.length === 'number');
 });
 
 test('--save', async t => {
-	const ret = await execa.shell('node ./bin/index.js --save pln usd eur chf');
+	const ret = await execa('./bin/index.js', ['--save', 'pln', 'usd', 'eur', 'chf']);
 	t.regex(ret.stdout, /Saved default currencies to/);
 });
 
 test('Test --save output without currencies', async t => {
-	const ret = await execa.shell('node ./bin/index.js --save');
+	const ret = await execa('./bin/index.js', ['--save']);
 	t.regex(ret.stdout, /Saved default currencies to/);
 });
 
 test('Test conversion without API key', async t => {
 	const error = await t.throwsAsync(async () => {
-		await execa.shell('node ./bin/index.js 10 foo usd');
+		await execa('./bin/index.js', ['10', 'foo', 'usd']);
 	});
 
-	t.regex(error.message, /Fixer.io API key not found/);
+	t.regex(error.stdout, /Fixer.io API key not found/);
 });
 
 test('Test setting invalid API key', async t => {
 	const error = await t.throwsAsync(async () => {
-		await execa.shell('node ./bin/index.js --key foo');
+		await execa('./bin/index.js', ['--key', 'foo']);
 	});
 
-	t.regex(error.message, /Provided API key seems invalid/);
+	t.regex(error.stdout, /Provided API key seems invalid/);
 });

5c6aa59e68ab087e673a6383fbe54741cad608b1
Antoni Kepinski <xxczaki@pm.me>
2019-08-05 16:00:13 +0200

    Change post install message

--- bin/post-install.js
+++ bin/post-install.js
@@ -2,8 +2,8 @@ const chalk = require('chalk');
 
 // Installation message
 console.log(`\n
-    ${chalk.magenta.bold('Please run `cash --key [key]` to set your conversion API key!')}
-    ${chalk.magenta('Get it here for free: https://fixer.io/signup/free')}
+${chalk.magenta.bold('Thanks for installing cash-cli!')}
+${chalk.magenta('Run `cash --help` to get started')}
 
-    ${chalk.cyan('If you enjoy using this tool, consider supporting me on Patreon - https://patreon.com/akepinski')}
+${chalk.cyan('Also, if you enjoy using this tool, consider supporting me on Patreon - https://patreon.com/akepinski')}
 \n`);
